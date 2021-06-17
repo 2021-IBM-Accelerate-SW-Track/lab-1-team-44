6365ce498b3828e73eff216fdaee28adc8a465bf
kemvlem <asitimbaykemlem@gmail.com>
2021-06-16 21:09:24 -0400

    Finished working on addind to list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import Header from "./component/header"
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/header/header";
 
-function App() {
-  return (
-    <div className="App">
-    <Header/>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    listItems: [],
+    text: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      text: this.state.text,
+      id: Date.now(),
+    };
+    this.setState((state) => ({
+      listItems: state.listItems.concat(newItem),
+      text: "",
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <h4>Todo List</h4>
+        <ul>
+          {this.state.listItems.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="todo">What needs to be done?</label>
+          <input
+            id="todo"
+            name="text"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>Add todo</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
