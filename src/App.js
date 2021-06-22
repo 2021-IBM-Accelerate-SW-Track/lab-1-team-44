40b4e6fa35702f907712e92c7994d310c7420d4f
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-06-22 00:02:18 -0400

    should pass test cases, last commit contained a bug

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <form addtodo>
         <input type = "submit" value = "Submit" name="input"></input>
       </form>
-      <div data-testid="new-item-button">
+      <div name="new-item-button">
         <button type = "button" name="new-item-button">new-item-button</button>
       </div>
 
